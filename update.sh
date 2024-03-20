56a87ab3c21fb865959a07360d5cc81834501797
Stephen Handley <stephen.handley@gmail.com>
2024-03-20 13:46:03 -0700

    rerunning from local to test adnaan's PR

--- update.sh
+++ update.sh
@@ -1,3 +1,15 @@
+MODE=$1
+
+API_URL_PROD=https://api.vocode.dev/openapi.json
+API_URL_LOCAL=http://localhost:3000/openapi.json
+
+if [ "$MODE" == "local" ]; then
+  URL=$API_URL_LOCAL
+else
+  URL=$API_URL_PROD
+fi
+
+echo "Updating openapi.json from $URL"
 FILE=./fern/openapi/openapi.json
-curl https://api.vocode.dev/openapi.json -o $FILE
+curl $URL -o $FILE
 npx prettier $FILE --write
